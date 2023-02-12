f0741bd6185e048b256039b529aec802985520f4
Rosen Blagoev <rosen.blagoev@linkmobility.com>
2023-02-12 16:53:06 +0200

    added tests for the rest of the verbs

--- src/main/java/com/example/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/example/msscbreweryclient/web/client/BreweryClient.java
@@ -6,22 +6,35 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
 @ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
-public class BreweryCient {
+public class BreweryClient {
 
     public final String BEER_PATH = "/api/v1/beers/";
     private String apiHost;
     private final RestTemplate restTemplate;
 
-    public BreweryCient(RestTemplateBuilder restTemplateBuilder) {
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
     public BeerDto getBeerById(UUID uuid) {
         return restTemplate.getForObject(apiHost + BEER_PATH + uuid.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apiHost + BEER_PATH, beerDto);
+    }
+
+    public void updateBeer(UUID uuid, BeerDto beerDto) {
+        restTemplate.put(apiHost + BEER_PATH + "/" + uuid.toString(), beerDto);
+    }
+
+    public void deleteBeer(UUID uuid) {
+        restTemplate.delete(apiHost + BEER_PATH + "/" + uuid);
     }
 
     public void setApiHost(String apiHost) {
--- src/test/java/com/example/msscbreweryclient/web/client/BreweryCientTest.java
+++ src/test/java/com/example/msscbreweryclient/web/client/BreweryCientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,10 +14,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class BreweryCientTest {
 
     @Autowired
-    private BreweryCient client;
+    private BreweryClient client;
     @Test
     void getBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Shumensko")
+                .build();
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateBeer() {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Shumensko")
+                .build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
     }
 }
